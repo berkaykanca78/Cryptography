b5d26665dbdb97faf87e7eb2fd33f1e68b1cb8b2
berkaykanca78 <berkaykanca@hotmail.com>
2018-10-20 00:58:56 +0300

    ProjectAdded

---
+++ ISSHW/ISSHW/FormMain.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace ISSHW
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtImagePath = new System.Windows.Forms.TextBox();
+            this.btnUploadImage = new System.Windows.Forms.Button();
+            this.btnKeyGenerator = new System.Windows.Forms.Button();
+            this.btnConvertImageToByte = new System.Windows.Forms.Button();
+            this.btnDecryptImage = new System.Windows.Forms.Button();
+            this.btnEncryptImage = new System.Windows.Forms.Button();
+            this.lblImageStatus = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.fileOpen = new System.Windows.Forms.OpenFileDialog();
+            this.pbExitWithoutDelete = new System.Windows.Forms.PictureBox();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.pbExitWithDelete = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbExitWithoutDelete)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbExitWithDelete)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtImagePath
+            // 
+            this.txtImagePath.Location = new System.Drawing.Point(163, 101);
+            this.txtImagePath.Name = "txtImagePath";
+            this.txtImagePath.ReadOnly = true;
+            this.txtImagePath.Size = new System.Drawing.Size(443, 22);
+            this.txtImagePath.TabIndex = 53;
+            this.txtImagePath.Text = "Image Path";
+            // 
+            // btnUploadImage
+            // 
+            this.btnUploadImage.Location = new System.Drawing.Point(612, 99);
+            this.btnUploadImage.Name = "btnUploadImage";
+            this.btnUploadImage.Size = new System.Drawing.Size(165, 27);
+            this.btnUploadImage.TabIndex = 52;
+            this.btnUploadImage.Text = "Upload Image";
+            this.btnUploadImage.UseVisualStyleBackColor = true;
+            this.btnUploadImage.Click += new System.EventHandler(this.btnUploadImage_Click);
+            // 
+            // btnKeyGenerator
+            // 
+            this.btnKeyGenerator.Location = new System.Drawing.Point(294, 463);
+            this.btnKeyGenerator.Name = "btnKeyGenerator";
+            this.btnKeyGenerator.Size = new System.Drawing.Size(165, 27);
+            this.btnKeyGenerator.TabIndex = 51;
+            this.btnKeyGenerator.Text = "Key Generator";
+            this.btnKeyGenerator.UseVisualStyleBackColor = true;
+            this.btnKeyGenerator.Click += new System.EventHandler(this.btnKeyGenerator_Click);
+            // 
+            // btnConvertImageToByte
+            // 
+            this.btnConvertImageToByte.Location = new System.Drawing.Point(122, 463);
+            this.btnConvertImageToByte.Name = "btnConvertImageToByte";
+            this.btnConvertImageToByte.Size = new System.Drawing.Size(165, 27);
+            this.btnConvertImageToByte.TabIndex = 50;
+            this.btnConvertImageToByte.Text = "Convert Image To Byte";
+            this.btnConvertImageToByte.UseVisualStyleBackColor = true;
+            this.btnConvertImageToByte.Click += new System.EventHandler(this.btnConvertImageToByte_Click);
+            // 
+            // btnDecryptImage
+            // 
+            this.btnDecryptImage.Location = new System.Drawing.Point(636, 463);
+            this.btnDecryptImage.Name = "btnDecryptImage";
+            this.btnDecryptImage.Size = new System.Drawing.Size(165, 27);
+            this.btnDecryptImage.TabIndex = 45;
+            this.btnDecryptImage.Text = "Decrypt Image";
+            this.btnDecryptImage.UseVisualStyleBackColor = true;
+            this.btnDecryptImage.Click += new System.EventHandler(this.btnDecryptImage_Click);
+            // 
+            // btnEncryptImage
+            // 
+            this.btnEncryptImage.Location = new System.Drawing.Point(465, 463);
+            this.btnEncryptImage.Name = "btnEncryptImage";
+            this.btnEncryptImage.Size = new System.Drawing.Size(165, 27);
+            this.btnEncryptImage.TabIndex = 44;
+            this.btnEncryptImage.Text = "Encrypt Image";
+            this.btnEncryptImage.UseVisualStyleBackColor = true;
+            this.btnEncryptImage.Click += new System.EventHandler(this.btnEncryptImage_Click);
+            // 
+            // lblImageStatus
+            // 
+            this.lblImageStatus.AutoSize = true;
+            this.lblImageStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblImageStatus.Location = new System.Drawing.Point(4, 6);
+            this.lblImageStatus.Name = "lblImageStatus";
+            this.lblImageStatus.Size = new System.Drawing.Size(153, 25);
+            this.lblImageStatus.TabIndex = 47;
+            this.lblImageStatus.Text = "[Image Status]";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTitle.Location = new System.Drawing.Point(172, 58);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(586, 25);
+            this.lblTitle.TabIndex = 46;
+            this.lblTitle.Text = "RSA IMAGE ENCRYPTION AND DECRYPTION PROGRAM";
+            // 
+            // fileOpen
+            // 
+            this.fileOpen.FileName = "fileOpen";
+            // 
+            // pbExitWithoutDelete
+            // 
+            this.pbExitWithoutDelete.Image = global::ISSHW.Properties.Resources.ExitWithoutDelete;
+            this.pbExitWithoutDelete.Location = new System.Drawing.Point(857, 6);
+            this.pbExitWithoutDelete.Name = "pbExitWithoutDelete";
+            this.pbExitWithoutDelete.Size = new System.Drawing.Size(35, 38);
+            this.pbExitWithoutDelete.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbExitWithoutDelete.TabIndex = 54;
+            this.pbExitWithoutDelete.TabStop = false;
+            this.pbExitWithoutDelete.Click += new System.EventHandler(this.pbExitWithoutDelete_Click);
+            // 
+            // pbImage
+            // 
+            this.pbImage.Image = global::ISSHW.Properties.Resources.Warning;
+            this.pbImage.Location = new System.Drawing.Point(306, 145);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(300, 300);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbImage.TabIndex = 49;
+            this.pbImage.TabStop = false;
+            // 
+            // pbExitWithDelete
+            // 
+            this.pbExitWithDelete.Image = global::ISSHW.Properties.Resources.ExitWithDelete;
+            this.pbExitWithDelete.Location = new System.Drawing.Point(898, 6);
+            this.pbExitWithDelete.Name = "pbExitWithDelete";
+            this.pbExitWithDelete.Size = new System.Drawing.Size(35, 38);
+            this.pbExitWithDelete.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbExitWithDelete.TabIndex = 48;
+            this.pbExitWithDelete.TabStop = false;
+            this.pbExitWithDelete.Click += new System.EventHandler(this.pbExitWithDelete_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(937, 496);
+            this.Controls.Add(this.txtImagePath);
+            this.Controls.Add(this.btnUploadImage);
+            this.Controls.Add(this.btnKeyGenerator);
+            this.Controls.Add(this.btnConvertImageToByte);
+            this.Controls.Add(this.btnDecryptImage);
+            this.Controls.Add(this.btnEncryptImage);
+            this.Controls.Add(this.lblImageStatus);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.pbExitWithoutDelete);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.pbExitWithDelete);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbExitWithoutDelete)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbExitWithDelete)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtImagePath;
+        private System.Windows.Forms.Button btnUploadImage;
+        private System.Windows.Forms.Button btnKeyGenerator;
+        private System.Windows.Forms.Button btnConvertImageToByte;
+        private System.Windows.Forms.Button btnDecryptImage;
+        private System.Windows.Forms.Button btnEncryptImage;
+        private System.Windows.Forms.Label lblImageStatus;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.OpenFileDialog fileOpen;
+        private System.Windows.Forms.PictureBox pbExitWithoutDelete;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.PictureBox pbExitWithDelete;
+    }
+}
+
---
+++ ISSHW/ISSHW/FormMain.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ISSHW
+{
+    public partial class FormMain : Form
+    {
+        public static byte[] ImageByteArray;
+        public static int[] IntArray;
+        public static int[] EncryptedArray;
+        public static int[] DecryptedArray;
+        public static int e;
+        public static int n;
+        public static int d;
+        public static int c;
+        public string FilePath = "";
+        public string OriginalUrl = Application.StartupPath + "\\Original.txt";
+        public string OriginalImageUrl = Application.StartupPath + "\\OriginalImage.png";
+        public string KeyGenerateUrl = Application.StartupPath + "\\KeyGenerate.txt";
+        public string EncryptUrl = Application.StartupPath + "\\Encrypted.txt";
+        public string EncryptedImageUrl = Application.StartupPath + "\\EncryptedImage.png";
+        public string DecryptUrl = Application.StartupPath + "\\Decrypted.txt";
+        public string DecryptedImageUrl = Application.StartupPath + "\\DecryptedImage.png";
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            File.Delete(OriginalUrl);
+            File.Delete(KeyGenerateUrl);
+            File.Delete(EncryptUrl);
+            File.Delete(DecryptUrl);
+            File.Delete(OriginalImageUrl);
+            File.Delete(EncryptedImageUrl);
+            File.Delete(DecryptedImageUrl);
+            FileStream original = File.Create(@""+OriginalUrl+"");
+            original.Close();
+            FileStream key = File.Create(@"" + KeyGenerateUrl + "");
+            key.Close();
+            FileStream encrypt = File.Create(@"" + EncryptUrl + "");
+            encrypt.Close();
+            FileStream decrypt = File.Create(@"" + DecryptUrl + "");
+            decrypt.Close();
+        }
+
+        public void UploadImage()
+        {
+            fileOpen.Title = "Choose Image";
+            fileOpen.Filter = "(*.bmp)|*.bmp|(*.png)|*.png|(*.jpg)|*.jpg|(*.gif)|*.gif";
+            fileOpen.ShowDialog();
+            FilePath = fileOpen.FileName;
+            txtImagePath.Text = FilePath;
+            pbImage.ImageLocation = FilePath;
+        }
+
+        private void btnUploadImage_Click(object sender, EventArgs e)
+        {
+            UploadImage();
+            lblImageStatus.Text = "[Image Was Uploaded]";
+        }
+
+        public byte[] ConvertImageToByteArray(System.Drawing.Image img)
+        {
+            MemoryStream ms = new MemoryStream();
+            img.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            return ms.ToArray();
+        }
+
+        private void btnConvertImageToByte_Click(object sender, EventArgs e)
+        {
+            ImageByteArray = new byte[0];
+            ImageByteArray = ConvertImageToByteArray(pbImage.Image);
+            FileStream fs = new FileStream(OriginalUrl, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            IntArray = ImageByteArray.Select(x => (int)x).ToArray();
+            for (int j = 0; j < ImageByteArray.Length; j++)
+            {
+                sw.Write(IntArray[j] + " ");
+            }
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+            File.WriteAllBytes(OriginalImageUrl, ImageByteArray);
+            lblImageStatus.Text = "[Image Was Converted From Image To Byte Array]";
+        }
+
+        public int InverseModulo(int Reverse, int Modulo)
+        {
+            int i = Modulo, v = 0, d = 1;
+            while (Reverse > 0)
+            {
+                int t = i / Reverse, x = Reverse;
+                Reverse = i % x;
+                i = x;
+                x = d;
+                d = v - t * x;
+                v = x;
+            }
+            v %= Modulo;
+            if (v < 0) v = (v + Modulo) % Modulo;
+            return v;
+        }
+
+        public int GcdControl(int less, int more)
+        {
+            Random r = new Random();
+            int gcd = 1;
+            int i;
+            for (i = 1; i <= more; i++)
+            {
+                if ((less % i == 0) && (more % i == 0))
+                {
+                    gcd = i;
+                }
+                if (gcd != 1)
+                {
+                    less = r.Next(2, more);
+                    i = 0;
+                }
+            }
+            return less;
+        }
+
+        public Tuple<int, int> RandomPrime(int num1, int num2)
+        {
+            Random r = new Random();
+            int[] numbers = new int[2] { num1, num2 };
+            int i;
+            int j;
+            for (j = 0; j <= 1; j++)
+            {
+                for (i = 2; i < numbers[j]; i++)
+                {
+                    int counter = 0;
+                    if (numbers[j] != 2)
+                    {
+                        if (numbers[j] % i == 0)
+                        {
+                            counter++;
+                        }
+                        if (counter > 0)
+                        {
+                            numbers[j] = r.Next(17, 98);
+                            j = 0;
+                            i = 1;
+                            if (numbers[0] == numbers[1])
+                            {
+                                numbers[j] = r.Next(17, 98);
+                                j = 0;
+                                i = 1;
+                                if (numbers[0] % 2 == 0 || numbers[1] % 2 == 0)
+                                {
+                                    numbers[j] = r.Next(17, 98);
+                                    j = 0;
+                                    i = 1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            num1 = numbers[0];
+            num2 = numbers[1];
+            return new Tuple<int, int>(num1, num2);
+        }
+
+        public void KeyGenerate()
+        {
+            FileStream fs = new FileStream(KeyGenerateUrl, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            Random r = new Random();
+            int p = r.Next(17, 98);
+            int q = r.Next(17, 98);
+            var tuple = RandomPrime(p, q);
+            p = tuple.Item1;
+            q = tuple.Item2;
+            n = p * q;
+            int totient = (p - 1) * (q - 1);
+            e = r.Next(2, totient);
+            e = GcdControl(e, totient);
+            d = InverseModulo(e, totient);
+            sw.WriteLine("p=" + p);
+            sw.WriteLine("q=" + q);
+            sw.WriteLine("n=" + n);
+            sw.WriteLine("totient=" + totient);
+            sw.WriteLine("e=" + e);
+            sw.WriteLine("d=" + d);
+            sw.WriteLine("Public Keys:" + " n=" + n + " e=" + e);
+            sw.WriteLine("Private Keys:" + " n=" + n + " d=" + d);
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+        }
+
+        private void btnKeyGenerator_Click(object sender, EventArgs e)
+        {
+            KeyGenerate();
+            lblImageStatus.Text = "[Keys Was Generated]";
+        }
+
+        public int Encrypt(int m)
+        {
+            int temp = m;
+            for (int i = 1; i < e; i++)
+            {
+                m *= temp;
+                m %= n;
+            }
+            c = m;
+            return c;
+        }
+
+        public int Decrypt(int c)
+        {
+            int des;
+            int temp = c;
+            for (int i = 1; i < d; i++)
+            {
+                c *= temp;
+                c %= n;
+            }
+            des = c;
+            return des;
+        }
+
+        private void btnEncryptImage_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream(EncryptUrl, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            EncryptedArray = new int[IntArray.Length];
+            for (int j = 0; j < ImageByteArray.Length; j++)
+            {
+                EncryptedArray[j] = Encrypt(IntArray[j]);
+                sw.Write(EncryptedArray[j] + " ");
+            }
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+            for (int i = 0; i < ImageByteArray.Length; i++)
+            {
+                ImageByteArray[i] = (byte)EncryptedArray[i];
+            }
+            File.WriteAllBytes(EncryptedImageUrl, ImageByteArray);
+            lblImageStatus.Text = "[Image Was Encrypted]";
+        }
+
+        private void btnDecryptImage_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream(DecryptUrl, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            DecryptedArray = new int[EncryptedArray.Length];
+            for (int j = 0; j < ImageByteArray.Length; j++)
+            {
+                DecryptedArray[j] = Decrypt(EncryptedArray[j]);
+                sw.Write(DecryptedArray[j] + " ");
+            }
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+            for (int i = 0; i < ImageByteArray.Length; i++)
+            {
+                ImageByteArray[i] = (byte)DecryptedArray[i];
+            }
+            File.WriteAllBytes(DecryptedImageUrl, ImageByteArray);
+            lblImageStatus.Text = "[Image Was Decrypted]";
+        }
+
+        private void pbExitWithoutDelete_Click(object sender, EventArgs e)
+        {
+            MessageBoxManager.Unregister();
+            MessageBoxManager.Yes = "Yes";
+            MessageBoxManager.No = "No";
+            MessageBoxManager.Register();
+            DialogResult choose = MessageBox.Show("Are You Sure To Exit Without Delete Text And Image Files?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (choose == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void pbExitWithDelete_Click(object sender, EventArgs e)
+        {
+            MessageBoxManager.Unregister();
+            MessageBoxManager.Yes = "Yes";
+            MessageBoxManager.No = "No";
+            MessageBoxManager.Register();
+            DialogResult choose = MessageBox.Show("Are You Sure To Exit With Delete Text And Image Files?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (choose == DialogResult.Yes)
+            {
+                File.Delete(OriginalUrl);
+                File.Delete(KeyGenerateUrl);
+                File.Delete(EncryptUrl);
+                File.Delete(DecryptUrl);
+                File.Delete(DecryptedImageUrl);
+                File.Delete(EncryptedImageUrl);
+                File.Delete(OriginalImageUrl);
+                Application.Exit();
+            }
+        }
+    }
+}
